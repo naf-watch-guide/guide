0cc1649f08e331049112a79a5b07bd7f50f76946
spencers145 <smartypantsspencer@gmail.com>
2025-08-20 10:10:50 -0400

    Fix Wunba

--- js/main.js
+++ js/main.js
@@ -170,10 +170,11 @@ function main() {
         if (linkobj.links.length !== 0) {
             if (linkobj.username === "rekrap2") {
                 $("#names").append(`<td class="user">${linkobj.username}'s Story<br><b>(RECOMMENDED)</td>`)
+            } else if (linkparams.season === 1 && linkobj.username === "Wunba") {
+                $("#names").append(`<td class="user">${linkobj.username}'s Story<br><i>(COMPILATION)</td>`)
             } else {
                 $("#names").append(`<td class="user">${linkobj.username}'s Story</td>`)
             }
-            
         }
     });
 
