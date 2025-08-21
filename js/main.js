e1d08ae50c54a5ac50c1786e3e80a9b95e1df372
spencers145 <smartypantsspencer@gmail.com>
2025-08-21 10:52:19 -0400

    Sound Effects for Advancing Video Stages

--- js/main.js
+++ js/main.js
@@ -177,11 +177,23 @@ function main() {
             iamwatching.progress += 1
         }
         $("#overlay").removeClass("overlay-visible")
-        sound = new Audio(`assets/sound/click.ogg`)
-        sound.volume = 0.18
-        sound.play()
         regen()
         save()
+
+        if (iamwatching.progress !== iamwatching.ending) {
+            sound = new Audio(`assets/sound/click.ogg`)
+            sound.volume = 0.18
+            sound.play()
+            if (iamwatching.progress !== 8) {
+                sound = new Audio(`assets/sound/levelup.ogg`)
+                sound.volume = 0.04
+                sound.play()
+            }
+        } else {
+            sound = new Audio(`assets/sound/fallbig.ogg`)
+            sound.volume = 0.14
+            sound.play()
+        }
     })
 
     $("#overlay-no").on("click", () => {
