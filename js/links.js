6307bcf7d673af92f0fedfe4a2fa2ace36d9f265
spencers145 <smartypantsspencer@gmail.com>
2026-09-26 12:12:06 -0400

    Final S3 Links

--- js/links.js
+++ js/links.js
@@ -41,7 +41,9 @@ links_s3 = [
     { username: "BranzyCraft", links: ["nVEXyf1bMxQ", "LJfYKIk5CNU"], progress: 0 },
     { username: "jojosolos", links: ["xhWEx7Bl1PU", "BYmpThQ8GDI"], progress: 0 },
     { username: "Legundo", links: ["dH7R3B7FITg", "rrCegqf6mrY"], progress: 0 },
+    { username: "Loony", links: ["MieMNjT9sAY"], progress: 0 },
     { username: "rekrap", links: ["ywehA3HqxZ8", "BW2FRBEOCoE"], progress: 0 },
     { username: "Taneesha­hogan", links: ["XB4LbuPT4XA", "rqUQdVEjcAQ"], progress: 0 },
-    { username: "SolidarityGaming", links: ["dTgSFZRT4m8"], progress: 0 }
+    { username: "SolidarityGaming", links: ["dTgSFZRT4m8"], progress: 0 },
+    { username: "Wunba", links: ["JvPidZhb1mQ"], progress: 0 },
 ]
